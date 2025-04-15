2cca47c48c1b9206a571fc3b2f52bdc48999a06f
Alejandrette <alejandrette5117@gmail.com>
2025-04-15 16:04:41 +0200

    add nav menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import Logo from "./Logo";
+import { NavMenu } from "./NavMenu";
 
 export default function Header() {
   return (
     <header className="bg-blue-950 p-5">
-      <Logo />
+      <div className="max-w-screen-2xl mx-auto flex flex-row justify-between items-center">
+        <div>
+          <Logo />
+        </div>
+        <NavMenu />
+      </div>
     </header>
   )
 }
